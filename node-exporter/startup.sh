49032dc51fce061def1878715e37dca146a63105
Zoltan Mosonyi <mosonyi@esix.hu>
2023-11-08 14:02:14 +0100

    Fix set hostname

--- node-exporter/startup.sh
+++ node-exporter/startup.sh
@@ -10,4 +10,6 @@ fi
 NODE_NAME=$(cat /etc/nodename)
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /node-meta.prom
 
+hostname ${NODE_NAME} || echo "add cap_add: - SYS_ADMIN" && sleep 10 && exit 1
+
 /bin/node_exporter --collector.textfile.directory=/
