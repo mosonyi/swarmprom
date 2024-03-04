bc3dc2fb3c0072648a8710d1bf1372db9d98d4c6
Zoltan Mosonyi <mosonyi@esix.hu>
2024-03-04 11:42:51 +0100

    Try to fix node-exporter

--- node-exporter/conf/docker-entrypoint.sh
+++ node-exporter/conf/docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -e
 
+if [ "${NODE_ID}" = "" ]; then
+    echo "ERROR: NODE_ID ENV is required for this container to run"
+    exit 1
+fi
+
 NODE_NAME=$(cat /etc/nodename)
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
+
+hostname ${NODE_NAME} || echo "add cap_add: - SYS_ADMIN"
 
 set -- /bin/node_exporter "$@"
 
